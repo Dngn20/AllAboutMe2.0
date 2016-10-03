92297512f8c0d6a2fc57278d02cf89619f4e7aff
Dngn20 <dngu9879@740s-pl-998.local>
2016-10-03 13:33:31 -0600

    Add a segue way to home to index and made titles for all of my screens

---
+++ AllAboutMe2.0/IndexViewController.swift
@@ -0,0 +1,20 @@
+//
+//  IndexViewController.swift
+//  AllAboutMe2.0
+//
+//  Created by Nguyen, Duncan on 10/3/16.
+//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
+//
+
+import UIKit
+
+class IndexViewController : UIViewController {
+    
+    override internal func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
