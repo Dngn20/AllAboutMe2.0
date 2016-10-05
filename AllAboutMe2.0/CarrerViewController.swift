52dface2cbbaf3f4d54265c170a1a7f17305f437
Dngn20 <dngu9879@740s-pl-998.local>
2016-10-05 12:11:01 -0600

    Add segue ways to all the ViewControllers and add some ViewControllers

---
+++ AllAboutMe2.0/CarrerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CarrerViewController.swift
+//  AllAboutMe2.0
+//
+//  Created by Nguyen, Duncan on 10/5/16.
+//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
+//
+
+import UIKit
+
+class CareerViewController : UIViewController
+{
+    
+}
---
+++ AllAboutMe2.0/FamilyViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FamilyViewController.swift
+//  AllAboutMe2.0
+//
+//  Created by Nguyen, Duncan on 10/5/16.
+//  Copyright © 2016 Nguyen, Duncan. All rights reserved.
+//
+
+import UIKit
+
+class FamilyViewController : UIViewController
+{
+    
+}
